678b0f0166466be2fa969dff2f2683913116fb20
DevanshuKumawat <devanshukumawat063@gmail.com>
2024-06-20 20:22:38 +0530

    addd

---
+++ javascript/js/Ternary.js
@@ -0,0 +1,28 @@
+// let age = 'FGHJKL';
+
+// age>18 ? console.log("You are eligible for vote")
+// : age <18 ? console.log("You are not eligible for vote")
+// : console.log("Invalid input")
+
+
+// SWITCH STATEMENT
+
+// let job_type = "Drivedfghjr"
+
+// switch(job_type){
+//     case "Teacher":
+//         console.log("You are teacher")
+//         break
+
+//     case "Doctor":
+//         console.log("You are doctor")
+//         break
+     
+//     case "Driver":
+//         console.log("You are driver") 
+//         break 
+       
+//     default:
+//         console.log("Invalid Input")  
+//         break  
+// }
--- javascript/js/object.js
+++ javascript/js/object.js
@@ -26,6 +26,20 @@ const product1 = {
     address:{
         city:"JAipur",
         pin_code:78778
-    }
+    },
+
+    allDeatials:function (){
+        console.log(`product name :- ${this.name} product price :- ${this.price} product desc :- ${this.desc}`)
+    }       
 }
+
+product1.allDeatials()
+
+// console.log(product1.address.city)
+
+// const {name,price,desc} = product1
+
+// console.log(name,price,desc)
+
+
 
